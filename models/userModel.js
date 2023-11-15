4b9e19f674d8d90997814b52ccdec4c83279bea5
Alex Dingal <lhexie2014@gmail.com>
2023-11-15 17:18:07 +0800

    Create initial user model

---
+++ models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter your name.'],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email.'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email.'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password.'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password.'],
+    validate: {
+      validator: function (el) {
+        return el === this.password
+      },
+      message: 'Password are not the same.',
+    },
+  },
+  lastLoggedIn: {
+    type: Date,
+    default: Date.now(),
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  // Hide version key
+  __v: {
+    type: Number,
+    select: false,
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
