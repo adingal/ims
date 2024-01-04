07c8c77f6f6762c1b19c14cad500b266977e9a91
Alex Dingal <lhexie2014@gmail.com>
2024-01-04 16:17:19 +0800

    Integrate loadboard routes to app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const userRouter = require('./routes/userRoutes')
+const loadBoardRouter = require('./routes/loadBoardRoutes')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 
@@ -21,6 +22,7 @@ app.use(express.json({ limit: '10kb' }))
 
 // Routes
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/loadboards', loadBoardRouter)
 
 // Catch all route handler
 app.all('*', (req, res, next) => {
