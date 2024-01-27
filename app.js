c8779cbe0b66350e3b2f2cebc03a9fd776ddd6e4
Alex Dingal <lhexie2014@gmail.com>
2024-01-27 15:43:26 +0800

    Install and configure security packages

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
+
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
+
 const userRouter = require('./routes/userRoutes')
 const loadBoardRouter = require('./routes/loadBoardRoutes')
 const historyRouter = require('./routes/historyRoutes')
-const AppError = require('./utils/appError')
-const globalErrorHandler = require('./controllers/errorController')
 
 // App
 const app = express()
@@ -18,8 +26,39 @@ if (process.env.NODE_ENV === 'development') {
 // Set public route
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Set security HTTP headers
+app.use(helmet())
+
+// Limit request for 100 per hour same IP
+const limiter = rateLimit({
+  max: 999,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour.',
+})
+app.use('/api', limiter)
+
 // Parse incoming request to JSON
 app.use(express.json({ limit: '10kb' }))
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XSS
+app.use(xss())
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+)
 
 // Routes
 app.use('/api/v1/users', userRouter)
